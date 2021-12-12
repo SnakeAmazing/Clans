8c86b2270e98f0e83e3e7331fd0a2b5f589ff8c4
SnakeAmazing <lluisponsvidal@gmail.com>
2021-12-12 20:10:13 +0100

    as

--- src/main/java/me/snakeamazing/clans/storage/repository/RepositoryMatcher.java
+++ src/main/java/me/snakeamazing/clans/storage/repository/RepositoryMatcher.java
@@ -1,6 +1,31 @@
 package me.snakeamazing.clans.storage.repository;
 
+import dev.morphia.Datastore;
+import me.snakeamazing.clans.clan.Clan;
+import me.snakeamazing.clans.provider.Provider;
+import me.snakeamazing.clans.storage.objects.Model;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class RepositoryMatcher {
 
+    private final Map<String, Provider<ObjectRepository<? extends Model>>> repositories = new HashMap<>();
 
+    private final Datastore datastore;
+
+    public RepositoryMatcher(Datastore datastore) {
+        this.datastore = datastore;
+
+        init();
+    }
+
+    public void init() {
+
+        Provider<ObjectRepository<Clan>> clanObjectProvider = new ObjectRepositoryProvider<>(datastore, Clan.class);
+
+        repositories.put("clans", clanObjectProvider);
+    }
+
+    
 }
